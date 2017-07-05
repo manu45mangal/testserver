e88548c52623d4f52d94d09bcadc26b10e303f94
Manu Mangal <manu45mangal@gmail.com>
2017-07-05 14:36:43 +0530

    config done

--- server.js
+++ server.js
@@ -2,10 +2,12 @@
  * Created by AMAR on 7/5/2017.
  */
 const express = require('express');
-
+const mongoose = require("mongoose");
 var app = express();
+var port = process.env.PORT|| 3000
+mongoose.connect("mongodb://manu:manu12345@ds149382.mlab.com:49382/project")
 
-app.listen(process.env.PORT,()=>{
+app.listen(port,()=>{
     console.log("Server is up")
 })
 
---
+++ test.js
@@ -0,0 +1,4 @@
+/**
+ * Created by AMAR on 7/5/2017.
+ */
+console.log(process.env)
